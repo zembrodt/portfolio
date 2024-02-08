e16a52ad702f7c3fb80c6eda8651cc48f2eca5dc
Ryan Zembrodt <ryan.zembrodt@gmail.com>
2024-02-08 00:44:33 -0500

    Upgrade remaining dependencies to Angular 15

--- src/app/core/timeline/timeline/timeline.component.ts
+++ src/app/core/timeline/timeline/timeline.component.ts
@@ -11,7 +11,7 @@ import {TimelineEntryComponent} from '../timeline-entry/timeline-entry.component
 import {Subscription} from 'rxjs';
 import {TimelineDividerComponent} from '../timeline-divider/timeline-divider.component';
 import {TimelineDialog} from './timeline.dialog';
-import {MatDialog} from '@angular/material/dialog';
+import {MatLegacyDialog as MatDialog} from '@angular/material/legacy-dialog';
 
 const DIALOG_ID = 'portfolio-zembrodt-dialog-id';
 const NAVBAR_PADDING = 12;
