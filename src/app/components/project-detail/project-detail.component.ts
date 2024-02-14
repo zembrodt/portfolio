8e2e4c038703294c0a192153ff56930f57c5dfc0
Ryan Zembrodt <ryan.zembrodt@gmail.com>
2024-02-13 22:16:03 -0500

    #16 Project screenshots can now be popped-out in a dialog when clicked on

--- src/app/components/project-detail/project-detail.component.ts
+++ src/app/components/project-detail/project-detail.component.ts
@@ -1,21 +1,37 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnDestroy, TemplateRef} from '@angular/core';
 import {Project} from '../projects/projects.component';
 import {faGithub} from '@fortawesome/free-brands-svg-icons';
 import {Select} from '@ngxs/store';
 import {ScreenState} from '../../core/screen/screen.state';
 import {Observable} from 'rxjs';
+import {MatDialog, MatDialogRef} from '@angular/material/dialog';
 
 @Component({
   selector: 'app-project-detail',
   templateUrl: './project-detail.component.html',
   styleUrls: ['./project-detail.component.scss']
 })
-export class ProjectDetailComponent {
+export class ProjectDetailComponent implements OnDestroy {
+
+  screenshotDialogRef: MatDialogRef<any>;
 
   @Select(ScreenState.isLtMd) isLtMd$!: Observable<boolean>;
   @Input() project: Project;
 
   githubIcon = faGithub;
 
-  constructor() {}
+  constructor(private dialog: MatDialog) {}
+
+  openScreenshotDialog(template: TemplateRef<unknown>): void {
+    this.screenshotDialogRef = this.dialog.open(template, {
+      maxHeight: '90vh',
+      maxWidth: '90vw'
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.screenshotDialogRef) {
+      this.screenshotDialogRef.close();
+    }
+  }
 }
